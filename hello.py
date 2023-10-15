507c41ba8d222bddc1285c278652de17c4d0bad8
purvakshi <121972586+purvakshi16@users.noreply.github.com>
2023-10-16 00:30:43 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,22 @@
+# what can be printed by print function!
+
+print("hello world")
+print("5")
+print(10)
+print(10-5)
+print(10*5)
+print(5,5,10,100)
+
+# COMMENT , ESCAPE SEQUENCE , PRINT STATEMENT
+
+# hashtagg is used for SINGLE LINE COMMENT
+
+# select the text and press ( CTRL + / ) for MULTI LINE COMMENT.
+'''
+use 3 times single quotes for MULTI LINE COMMENT
+'''
+# \n [black-slash n] is used to chnage the line of a Statement 
+print("Hello , My name is purvakshi\nI am 20 years old ")
+
+print( 5,5,10,100 , sep = "^" , end = " bye\n")
+print("new line")
