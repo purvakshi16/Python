d36082df1dbe679b6572023729b5e4e16a4a788c
purvakshi <121972586+purvakshi16@users.noreply.github.com>
2024-09-01 20:40:31 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,12 @@
+temp = 9
+
+if temp > 30:
+    print("it's a hot day")
+    print("drink plenty of water")
+elif temp >20 :             # (temp between 20 - 30)
+    print("it's a nice day")
+elif temp >10:             # (temp between 10 - 20)
+    print("it's a bit cold")
+else:
+    print("it's cold")
+#print("done")
---
+++ strings.py
@@ -0,0 +1,7 @@
+'''
+anything that is enclosed between double and single quotation mark is considered as string
+'''
+
+name = "purvakshi"
+print("Hello,",name)
+
